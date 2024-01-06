b219f37a861fcfe9380f37f5803c4f958676f2d0
antoine-cha <antoine.chassang0@gmail.com>
2024-01-06 10:44:15 +0000

    Init aliases file

---
+++ dotfiles/private_dot_config/aliases/common.sh
@@ -0,0 +1,11 @@
+# Common aliases for all machines
+alias cm="chezmoi"
+
+# Aliases with eza
+# alias l='eza -lah --color=always --icons --group-directories-first' # with headers
+# alias la='eza -al --color=always --icons --group-directories-first'  # all files and dirs
+# alias ll='eza -l --color=always --icons --group-directories-first'  # long format
+# alias lt='eza -aT --color=always --icons --group-directories-first' # tree listing
+
+# # Aliases with fzf: with preview
+# alias fzp="fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'"
