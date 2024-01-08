263b139f65818411a1a7e9c895902b8b89af631d
antoine-cha <antoine.chassang0@gmail.com>
2024-01-08 23:19:46 +0000

    Clean utils install

--- dotfiles/private_dot_config/aliases/common.sh
+++ dotfiles/private_dot_config/aliases/common.sh
@@ -2,10 +2,10 @@
 alias cm="chezmoi"
 
 # Aliases with eza
-# alias l='eza -lah --color=always --icons --group-directories-first' # with headers
-# alias la='eza -al --color=always --icons --group-directories-first'  # all files and dirs
-# alias ll='eza -l --color=always --icons --group-directories-first'  # long format
-# alias lt='eza -aT --color=always --icons --group-directories-first' # tree listing
+alias l='eza -lah --color=always --icons --group-directories-first' # with headers
+alias la='eza -al --color=always --icons --group-directories-first'  # all files and dirs
+alias ll='eza -l --color=always --icons --group-directories-first'  # long format
+alias lt='eza -aT --color=always --icons --group-directories-first' # tree listing
 
-# # Aliases with fzf: with preview
-# alias fzp="fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'"
+# Aliases with fzf: with preview
+alias fzp="fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'"
